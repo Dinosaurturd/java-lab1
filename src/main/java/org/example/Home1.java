a498d9b59bab1d1a97c0d33677e0093be8005e2b
Dinosaurturd <zhenya.aleksandrov05@gmail.com>
2025-04-13 21:02:41 +0700

    Aboba

---
+++ src/main/java/org/example/Home1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Home1 {
+    public static String FizzBuzz(int i) {
+        if (i % 5 == 0 && i % 7 == 0) {
+            return "fizzbuzz";
+        } else if (i % 5 == 0) {
+            return "fizz";
+        } else if (i % 7 == 0) {
+            return "buzz";
+        } else {
+            return Integer.toString(i);
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,35 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.Home1;
+import org.example.Home2;
+import org.example.Home3;
+import org.example.Home4;
+import org.example.Home5;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Task 1: FizzBuzz");
+        for (int i = 1; i <= 20; i++) { // Check the first 20 numbers
+            System.out.println(Home1.FizzBuzz(i));
         }
+
+        System.out.println("\nTask 2: Reverse a string");
+        String word = "make install";
+        System.out.println("Original string: " + word);
+        System.out.println("Reversed: " + Home2.make_install(word));
+
+        System.out.println("\nTask 3: Quadratic equation");
+        int a = 1, b = 4, c = 4;
+        System.out.println(a + "x^2 + " + b + "x + " + c + " = 0");
+        System.out.println(Home3.Equation(a, b, c));
+
+        System.out.println("\nTask 4: Sum of a series");
+        double sum = Home4.Calc();
+        System.out.println("Sum of the series: " + sum);
+
+        System.out.println("\nTask 5: Palindrome check");
+        String testWord = "Home";
+        System.out.println("Word: " + testWord);
+        System.out.println("Is it a palindrome? " + Home5.Palindrome(testWord));
     }
 }
